8cf2bb7791ef01a9c89086873565bf5796b856e6
Ken <kenwong0@users.noreply.github.com>
2015-12-07 13:02:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -98,23 +98,22 @@ app.delete('/restaurant_id/:id/grade/:date/:grade/:score',function(req,res) {
 				throw err
 			}
 			if (results.length > 0) {
-			db.once('open', function (callback) {
 				Restaurant.update({restaurant_id: req.params.id},{$pull:{grades:{date:req.params.date,grade:req.params.grade,score:req.params.score}}},{ safe: true },function(err){
 			if (err) {
 				res.status(500).json(err);
 				throw err
 			}else{
 				res.status(200).json({message: 'delete done'});
+				db.close();
 			}
-db.close();
 			
 		});
-}
 			}
 			else {
 				res.status(200).json({message: 'No matching document'});
+				db.close();
 			}
-			db.close();
+			
     	});
 		
     });
