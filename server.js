3174a2c48316fde5ea1664890a0fe8f0ba0346be
Ken <kenwong0@users.noreply.github.com>
2015-12-07 11:35:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,17 +16,21 @@ app.post('/',function(req,res) {
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function (callback) {
 		var rObj = {};
+		if(req.body.building != undefined||req.body.street != undefined||req.body.zipcode != undefined){
 		rObj.address = {};
-		rObj.address.building = req.body.building;
-		rObj.address.street = req.body.street;
-		rObj.address.zipcode = req.body.zipcode;
+		if(req.body.building != undefined) rObj.address.building = req.body.building;
+		if(req.body.street != undefined) rObj.address.street = req.body.street;
+		if(req.body.zipcode != undefined) rObj.address.zipcode = req.body.zipcode;
+		}
+		if(req.body.lon != undefined||req.body.lat != undefined){
 		rObj.address.coord = [];
 		rObj.address.coord.push(req.body.lon);
 		rObj.address.coord.push(req.body.lat);
-		rObj.borough = req.body.borough;
-		rObj.cuisine = req.body.cuisine;
-		rObj.name = req.body.name;
-		rObj.restaurant_id = req.body.restaurant_id;
+		}
+		if(req.body.borough != undefined) rObj.borough = req.body.borough;
+		if(req.body.cuisine != undefined) rObj.cuisine = req.body.cuisine;
+		if(req.body.name != undefined) rObj.name = req.body.name;
+		if(req.body.restaurant_id != undefined) rObj.restaurant_id = req.body.restaurant_id;
 
 		var Restaurant = mongoose.model('Restaurant', restaurantSchema);
 		var r = new Restaurant(rObj);
